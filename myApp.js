28f134986c9992f42fd5a29ca7a2f5db2505fc73
Manuel Araujo Baño <manuaraba@gmail.com>
2024-10-29 18:11:48 +0100

    Untrusted HTML

--- myApp.js
+++ myApp.js
@@ -11,14 +11,7 @@ app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 //prevent XSS protection
 app.use(helmet.xssFilter());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'"], // Asegúrate de ajustar según tus necesidades
-    objectSrc: ["'none'"],
-    upgradeInsecureRequests: [],
-  }
-}));
+
 //prevent MIME sniffing
 app.use(helmet.noSniff());
 
